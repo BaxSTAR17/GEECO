36452ac34fe025e683db5f09423c31a719de7851
FextraBlitz <lolanamol@gmail.com>
2025-10-20 00:00:46 +0800

    uhmm

--- lib/pages/rootpage.dart
+++ lib/pages/rootpage.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lazy_load_indexed_stack/lazy_load_indexed_stack.dart';
 import 'package:geeco/modules/globalbottomnav.dart';
 import 'package:geeco/pages/homepage.dart';
 import 'package:geeco/pages/scans.dart';
 import 'package:geeco/pages/editor.dart';
 import 'package:geeco/pages/settings.dart';
-import 'package:geeco/pages/about.dart';
-import 'package:geeco/pages/welcome.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -16,13 +15,13 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int _selectedIndex = 0;
+  double appBarBorderRadius = 100000.0;
 
-  final List<Widget> _pages = const [
+  final List<Widget> _pages = [
     HomePage(),
     ScansPage(),
     EditorPage(),
     SettingsPage(),
-    AboutUs(),
   ];
 
   final List<String> _pageNames = const [
@@ -30,48 +29,51 @@ class _RootPageState extends State<RootPage> {
     "Eco-Lens",
     "Editor",
     "Settings",
-    "About Us",
   ];
 
-  void _onItemTapped(int index) {
+  void _onItemTapped(int index){
     setState(() {
       _selectedIndex = index;
+      if (index == 2) {
+        appBarBorderRadius = 0.0;
+      }
+      else {
+        appBarBorderRadius = 100000.0;
+      }
+      
     });
-  }
 
-  // add this method
-  void setSelectedIndex(int index) => setState(() => _selectedIndex = index);
+    
+      
+  }
+    
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // allow the app bar area to be drawn over the top so we can inset it
-      extendBodyBehindAppBar: true,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80),
-        child: SafeArea(
-          bottom: false,
-          child: Container(
-            // increase top to move the rounded bar down; decrease to move up
-            margin: const EdgeInsets.only(top: 10.0, left: 5.0, right: 5.0),
-            height: 54,
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
-            decoration: BoxDecoration(
-              color: const Color(0xFF022000),
-              borderRadius: BorderRadius.circular(50.0),
-            ),
-            alignment: Alignment.centerLeft, // align text to the left
-            child: Text(
-              _pageNames[_selectedIndex],
-              style: const TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ),
+      appBar: AppBar(
+        title: Center(
+          child:Text(
+            _pageNames[_selectedIndex], 
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: "Gabarito"
+            )
+          )
+        ),
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(appBarBorderRadius), bottomRight: Radius.circular(appBarBorderRadius)),
         ),
       ),
-      body: IndexedStack(index: _selectedIndex, children: _pages),
+      body: LazyLoadIndexedStack(
+        index: _selectedIndex,
+        autoDisposeIndexes: List.generate(_pages.length, (i) => i),
+        children: _pages,
+      ),
       bottomNavigationBar: GlobalBottomNavigationBar(
         selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
+        onItemTapped: _onItemTapped
       ),
     );
   }
